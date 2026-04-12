060e89ec1fa97eda23acbcd654b4dc7caab3f766
Oleksiy <oleksiy.smola@gmail.com>
2026-04-13 00:29:49 +0100

    reads KEO mapping now

--- spar.py
+++ spar.py
@@ -1,4 +1,5 @@
 import numpy as np
+import re
 
 wavenumberConversion = 33.7152537836138732499014581824370 # conversion factor to cm-1
 
@@ -68,7 +69,7 @@ class basicFunction:
             functionOutput *= self.primitiveFunctionList[i](q)
         return functionOutput
         
-def readBasicFunctions(basicFunctionInputFile: str, masses : np.ndarray) -> dict:
+def readBasicFunctions(basicFunctionInputFile: str) -> dict:
     with open(basicFunctionInputFile) as f:
         basicFunctionInput: str = f.read()
     basicFunctionsList: dict = {}
@@ -107,82 +108,110 @@ def readBasicFunctions(basicFunctionInputFile: str, masses : np.ndarray) -> dict
         #     basicFunctionsList[i + 1] = modeFunctionList
         #     modeHeaderIndex += lineBeingRead
     else:
-        if "mass" in basicFunctionMainHeader:
-            basicFunctionsList[0] = 1/masses # Masses are effectively "mode" 0
-            for i in range(numberOfModes):
-                modeFunctionList = {} # New list of functions for mode
-                modeFunctionList[0] = basicFunction("0 1 r 1 1") # Function 0 is always 1!
-                numberOfLinesForMode: int = int(basicFunctionInputLines[modeHeaderIndex].split()[-1])
-                lineBeingRead: int = 1
-                while lineBeingRead <= numberOfLinesForMode:
-                    modeFunctionList[lineBeingRead] = basicFunction(basicFunctionInputLines[modeHeaderIndex + lineBeingRead])
-                    lineBeingRead += 1
-                basicFunctionsList[i] = modeFunctionList
-                modeHeaderIndex += lineBeingRead
+        for i in range(numberOfModes):
+            modeFunctionList = {} # New list of functions for mode
+            modeFunctionList[0] = basicFunction("0 1 r 1 1") # Function 0 is always 1!
+            numberOfLinesForMode: int = int(basicFunctionInputLines[modeHeaderIndex].split()[-1])
+            lineBeingRead: int = 1
+            while lineBeingRead <= numberOfLinesForMode:
+                modeFunctionList[lineBeingRead] = basicFunction(basicFunctionInputLines[modeHeaderIndex + lineBeingRead])
+                lineBeingRead += 1
+            basicFunctionsList[i + 1] = modeFunctionList
+            modeHeaderIndex += lineBeingRead
     return basicFunctionsList
 
-# Labels for kinetic energy operator components
+# Keys for kinetic energy operator dictionaries
 kineticComponents = ["gvib", "grot", "gcor", "pseudo"]
+kineticComponentsRegex = "gvib|grot|gcor|pseudo" # REGEX expression to split KEO checkpoint file
 
-class operatorMap:
-    operatorType: str
-    operatorRank: int
-    componentIndices: dict = {}
-    componentCoefficients: dict = {}
-    functionIndices: dict = {}
+class kineticMapping:
+    kineticCoefficients: dict = {} # Dictionary where each entry is a vector
+    kineticComponentIndices: dict = {} # Dictionary where each entry is a matrix of integers (N_L x 2) where N_L is the number of terms for the vibrational/rotational/coriolis/pseudopotential part of the KEO
+    kineticBasicFunctionIndices: dict = {} # Dictionary where each entry is a matrix of indices for the basic functions (N_L x [3N-6+1]) with masses and (N_L x [3N-6]) without masses
+    massesIncluded: bool
     numberOfModes: int
-    containsMass: bool = False
 
-    def __init__(self, operatorMappingInput: str):
-        operatorMappingInputLines: list = operatorMappingInput.split("\nEND")[0].split("\n")
-        operatorMappingHeader: str = operatorMappingInputLines[0].lower()
-        self.operatorType = operatorMappingHeader.split()[1]
-        self.containsMass = "mass" in operatorMappingHeader
-        self.operatorRank = operatorRankMapping[self.operatorType]
-        self.numberOfModes = len(operatorMappingInputLines[2].split()) - self.operatorRank - 3 - self.containsMass
-        if self.operatorType == "kinetic":
-            pass
+    def __init__(self, kineticCheckpointFile: str, massesIncluded: bool = True):
+        with open(kineticCheckpointFile) as f:
+            kineticCheckpointContent: str = f.read().lower()
+        kineticCheckpointContentSplit = re.split(kineticComponentsRegex, kineticCheckpointContent)
+        kineticVibrationalInput = kineticCheckpointContentSplit[1].split("\n")[1:][:-2]
+        kineticRotationalInput = kineticCheckpointContentSplit[2].split("\n")[1:][:-2]
+        kineticCoriolisInput = kineticCheckpointContentSplit[3].split("\n")[1:][:-2]
+        kineticPseudopotentialInput = kineticCheckpointContentSplit[4].split("\nEnd of kinetic")[0].split("\n")[1:][:-4]
+        
+        self.massesIncluded = massesIncluded
+        self.numberOfModes = len(kineticVibrationalInput[0].split()) - self.massesIncluded - 5
+        
+        # Parse Vibrational input
+        self.kineticComponentIndices["gvib"] = np.zeros((len(kineticVibrationalInput), 2))
+        self.kineticCoefficients["gvib"] = np.zeros(len(kineticVibrationalInput))
+        self.kineticBasicFunctionIndices["gvib"] = np.zeros((len(kineticVibrationalInput), self.numberOfModes + self.massesIncluded))
+
+        for i in range(len(kineticVibrationalInput)):
+            componentLineSplit = kineticVibrationalInput[i].split()
+            self.kineticComponentIndices["gvib"][i] = np.array([int(componentLineSplit[0]), int(componentLineSplit[1])])
+            self.kineticCoefficients["gvib"][i] = float(componentLineSplit[4])
+            for j in range(self.numberOfModes + self.massesIncluded):
+                self.kineticBasicFunctionIndices["gvib"][i, j] = int(componentLineSplit[5 + j])
+
+        # Parse Rotational input
+        self.kineticComponentIndices["grot"] = np.zeros((len(kineticRotationalInput), 2))
+        self.kineticCoefficients["grot"] = np.zeros(len(kineticRotationalInput))
+        self.kineticBasicFunctionIndices["grot"] = np.zeros((len(kineticRotationalInput), self.numberOfModes + self.massesIncluded))
+
+        for i in range(len(kineticRotationalInput)):
+            componentLineSplit = kineticRotationalInput[i].split()
+            self.kineticComponentIndices["grot"][i] = np.array([int(componentLineSplit[0]), int(componentLineSplit[1])])
+            self.kineticCoefficients["grot"][i] = float(componentLineSplit[4])
+            for j in range(self.numberOfModes + self.massesIncluded):
+                self.kineticBasicFunctionIndices["grot"][i, j] = int(componentLineSplit[5 + j])
+
+        # Parse Coriolis input
+        self.kineticComponentIndices["gcor"] = np.zeros((len(kineticCoriolisInput), 2))
+        self.kineticCoefficients["gcor"] = np.zeros(len(kineticCoriolisInput))
+        self.kineticBasicFunctionIndices["gcor"] = np.zeros((len(kineticCoriolisInput), self.numberOfModes + self.massesIncluded))
+
+        for i in range(len(kineticCoriolisInput)):
+            componentLineSplit = kineticCoriolisInput[i].split()
+            self.kineticComponentIndices["gcor"][i] = np.array([int(componentLineSplit[0]), int(componentLineSplit[1])])
+            self.kineticCoefficients["gcor"][i] = float(componentLineSplit[4])
+            for j in range(self.numberOfModes + self.massesIncluded):
+                self.kineticBasicFunctionIndices["gcor"][i, j] = int(componentLineSplit[5 + j])
+
+        # Parse pseudopotential input
+        self.kineticComponentIndices["pseudo"] = np.zeros((len(kineticPseudopotentialInput), 2))
+        self.kineticCoefficients["pseudo"] = np.zeros(len(kineticPseudopotentialInput))
+        self.kineticBasicFunctionIndices["pseudo"] = np.zeros((len(kineticPseudopotentialInput), self.numberOfModes + self.massesIncluded))
+
+        for i in range(len(kineticPseudopotentialInput)):
+            componentLineSplit = kineticPseudopotentialInput[i].split()
+            self.kineticCoefficients["pseudo"][i] = float(componentLineSplit[4])
+            for j in range(self.numberOfModes + self.massesIncluded):
+                self.kineticBasicFunctionIndices["pseudo"][i, j] = int(componentLineSplit[5 + j])
+
+    def evaluate(self, kineticComponentLabel: str, basicFunctions: dict, internalCoordinates: np.ndarray, masses: np.ndarray):
+        numberOfComponentCoefficients = len(self.kineticCoefficients[kineticComponentLabel])
+        match kineticComponentLabel:
+            case "gvib":
+                kineticComponent = np.zeros((self.numberOfModes, self.numberOfModes))
+            case "grot":
+                kineticComponent = np.zeros((3, 3))
+            case "gcor":
+                kineticComponent = np.zeros((self.numberOfModes, 3))
+            case "pseudo":
+                kineticComponent = 0.0
+        if kineticComponentLabel == "pseudo":
+            for i in range(numberOfComponentCoefficients):
+                if self.massesIncluded:
+                    kineticComponent = self.kineticCoefficients[kineticComponentLabel][i]/masses[self.kineticBasicFunctionIndices[kineticComponentLabel][i, 0]]
+                for j in range(self.numberOfModes):
+                    kineticComponent *= basicFunctions[j][self.kineticBasicFunctionIndices[kineticComponentLabel][i, j + 1]].evaluate(internalCoordinates[j])
         else:
-            operatorComponentInputLines: list = operatorMappingInputLines[2 :]
-            numberOfComponentTerms: int = len(operatorComponentInputLines)
-            self.componentCoefficients[self.operatorType] = np.zeros(numberOfComponentTerms)
-            if self.operatorRank > 0:
-                self.componentIndices[self.operatorType] = np.zeros((numberOfComponentTerms, self.operatorRank))
-            self.functionIndices[self.operatorType] = np.zeros((numberOfComponentTerms, self.numberOfModes + self.containsMass))
-            for i in range(numberOfComponentTerms):
-                operatorComponentLineSplit: list = operatorComponentInputLines[i].split()
-                self.componentCoefficients[self.operatorType][i] = float(operatorComponentLineSplit[self.operatorRank + 2])
-                for j in range(self.operatorRank):
-                    self.componentIndices[self.operatorType][i, j] = int(operatorComponentLineSplit[j])
-                for j in range(self.numberOfModes + self.containsMass):
-                    self.functionIndices[self.operatorType][i, j] = int(operatorComponentLineSplit[j + self.operatorRank + 3])
-    
-    def evaluatePointOfComponent(self, component: str, basicFunctionsList: dict, q):
-        coefficients = self.componentCoefficients[component]
-        functionIndices = self.functionIndices[component]
-        numberOfTerms: int = len(coefficients)
-        terms = np.ones(numberOfTerms)
-        match self.operatorRank:
-            case 0:
-                for i in range(numberOfTerms):
-                    terms[i] *= coefficients[i]
-                    if self.containsMass:
-                        terms[i] *= basicFunctionsList[0][functionIndices[i, 0]]
-                        for j in range(1, self.containsMass + self.numberOfModes):
-                            terms[i] *= basicFunctionsList[j][functionIndices[i, j]].evaluate(q[j])
-                    else:
-                        for j in range(self.numberOfModes):
-                            terms[i] *= basicFunctionsList[j + 1][functionIndices[i, j]].evaluate(q[j])
-                return np.sum(terms)
-            case 1:
-                operatorValue = np.zeros(3)
-            case 2:
-                match component:
-                    case "pseudo":
-                        operatorValue = 0.0
-                    case "gvib":
-                        operatorValue = np.zeros((self.numberOfModes, self.numberOfModes))
-                    case "grot":
-                        operatorValue = np.zeros((3, 3))
-                    case "gcor":
-                        operatorValue = np.zeros((self.numberOfModes, 3))
+            for i in range(numberOfComponentCoefficients):
+                if self.massesIncluded:
+                    kineticComponent[self.kineticComponentIndices[kineticComponentLabel][i, 0], self.kineticComponentIndices[kineticComponentLabel][i, 1]] = self.kineticCoefficients[kineticComponentLabel][i]/masses[self.kineticBasicFunctionIndices[kineticComponentLabel][i, 0]]
+                for j in range(self.numberOfModes):
+                    kineticComponent[self.kineticComponentIndices[kineticComponentLabel][i, 0], self.kineticComponentIndices[kineticComponentLabel][i, 1]] *= basicFunctions[j][self.kineticBasicFunctionIndices[kineticComponentLabel][i, j + 1]].evaluate(internalCoordinates[j])
+        return kineticComponent*wavenumberConversion
+            
